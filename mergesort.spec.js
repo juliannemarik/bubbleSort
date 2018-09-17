aafd83f6f193383d1a62e2dd563c8d4b79b4991f
Lotus T <ltan93@gmail.com>
2018-09-17 15:06:28 -0500

    mergesort specs

--- mergesort.spec.js
+++ mergesort.spec.js
@@ -16,3 +16,36 @@ describe('Merge function', function() {
     expect(merge([3, 6, 14], [3, 6])).toEqual([3, 3, 6, 6, 14]);
   });
 });
+
+describe('mergeSort function', function() {
+  it('can sort a single index array', function() {
+    expect(mergeSort([1])).toEqual([1]);
+    expect(mergeSort([7423])).toEqual([7423]);
+  });
+  it('can sort an unsorted array', function() {
+    expect(mergeSort([1, 293, 42, 24])).toEqual([1, 24, 42, 293]);
+    expect(mergeSort([-3, 17, 13, 7, 83, 44])).toEqual([-3, 7, 13, 17, 44, 83]);
+  });
+  it('can handle negative values', function() {
+    expect(mergeSort([-134, -3, 0, -17, 30, -9])).toEqual([
+      -134,
+      -17,
+      -9,
+      -3,
+      0,
+      30,
+    ]);
+  });
+  it('can handle duplicate values in an unsorted array', function() {
+    expect(mergeSort([42, 42, 1, 62, 37])).toEqual([1, 37, 42, 42, 62]);
+    expect(mergeSort([33, 49, 33, 82, 74, 72, 74])).toEqual([
+      33,
+      33,
+      49,
+      72,
+      74,
+      74,
+      82,
+    ]);
+  });
+});
