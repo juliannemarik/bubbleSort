2a29cda7bbef81a614b7d55d49a6d882152ea603
Lotus T <ltan93@gmail.com>
2018-09-17 14:16:23 -0500

    merge working

--- mergesort.js
+++ mergesort.js
@@ -1,17 +1,26 @@
-function split (wholeArray) {
-  if (wholeArray.length === 1){
+function split(wholeArray) {
+  if (wholeArray.length === 1) {
     return wholeArray;
   }
-  let splitIdx = Math.ceil((wholeArray.length)/2);
-  return [wholeArray.slice(0, splitIdx), wholeArray.slice(splitIdx)]
+  let splitIdx = Math.ceil(wholeArray.length / 2);
+  return [wholeArray.slice(0, splitIdx), wholeArray.slice(splitIdx)];
 }
 
-function merge () {
-
+function merge(arr1, arr2, mergedArr = []) {
+  if (arr1.length === 0 || arr2.length === 0) {
+    let lastArr = arr1.length ? arr1 : arr2;
+    return mergedArr.concat(lastArr);
+  } else {
+    if (arr1[0] < arr2[0]) {
+      mergedArr.push(arr1.shift());
+    } else {
+      mergedArr.push(arr2.shift());
+    }
+    return merge(arr1, arr2, mergedArr);
+  }
 }
 
 function mergeSort() {
-
+  if (sortedArr1[0] < sortedArr2[0]) {
+  }
 }
-
-split([1,2,3,4]);
--- mergesort.spec.js
+++ mergesort.spec.js
@@ -1,7 +1,18 @@
 describe('Split Array function', function() {
   it('is able to split an array into two halves', function() {
     expect(split([1])).toEqual([1]);
-    expect(split([1, 2, 3, 4])).toEqual([[1, 2],[3, 4]]);
-    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2, 3],[4, 5]]);
+    expect(split([1, 2, 3, 4])).toEqual([[1, 2], [3, 4]]);
+    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2, 3], [4, 5]]);
+  });
+});
+
+describe('Merge function', function() {
+  it('is able to merge two sorted arrays into one sorted array', function() {
+    // test the merging algorithm
+    expect(merge([1], [2])).toEqual([1, 2]);
+    expect(merge([1, 2], [3, 4])).toEqual([1, 2, 3, 4]);
+    expect(merge([2, 4], [3, 5])).toEqual([2, 3, 4, 5]);
+    expect(merge([9], [5, 7])).toEqual([5, 7, 9]);
+    expect(merge([3, 6, 14], [3, 6])).toEqual([3, 3, 6, 6, 14]);
   });
 });
