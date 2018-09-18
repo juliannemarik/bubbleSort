663e366362cff88bef627ed8afef679cc82ccf0e
Julianne Crawford <julianne.marik@gmail.com>
2018-09-17 19:33:17 -0500

    comments added to bubble sort

--- bubblesort.js
+++ bubblesort.js
@@ -1,3 +1,7 @@
+// Function: bubbleSort()
+// ----------------------
+// Purpose: the bubbleSort function implements a bubble sort on an input array,
+// returning a sorted array
 function bubbleSort(unorderedArray, tracker = unorderedArray.length - 1) {
   if (tracker === 0 || tracker === -1){
     return unorderedArray;
@@ -8,11 +12,18 @@ function bubbleSort(unorderedArray, tracker = unorderedArray.length - 1) {
   return bubbleSort(unorderedArray, --tracker);
 }
 
+// Function: swap()
+// -----------------
+// Purpose: the swap function swaps two elements in an array based on
+// the bubble sort implementation
 function swap(i, unorderedArray) {
   [unorderedArray[i], unorderedArray[i + 1]] = [unorderedArray[i + 1], unorderedArray[i]];
   return unorderedArray;
 }
 
+// Function: compare()
+// -------------------
+// Purpose: the compare function compares the magnitude of two values in an array
 function compare(unorderedArray, i) {
   if (unorderedArray[i] >= unorderedArray[i + 1]) {
     swap(i, unorderedArray);
